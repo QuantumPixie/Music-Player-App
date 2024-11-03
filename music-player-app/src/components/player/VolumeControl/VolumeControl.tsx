a2b7170a1cb867c43e076f901d5206dca0efaa61
Tatjana Huppertz <tatjana.huppertz@gmail.com>
2024-11-03 11:40:09 +0000

    removed volume control for mobile view as not working properly

--- music-player-app/src/components/player/VolumeControl/VolumeControl.tsx
+++ music-player-app/src/components/player/VolumeControl/VolumeControl.tsx
@@ -1,48 +1,27 @@
 import { Volume2 } from "lucide-react";
 import Slider from "../../common/Slider/Slider";
-import { useVolumeControl } from "@/hooks/useVolumeControl";
+import { usePlayerStore } from "@/store/usePlayerStore";
 import "./VolumeControl.css";
 
 export const VolumeControl = () => {
-  const { volume, isOpen, controlRef, setIsOpen, handleVolumeChange } =
-    useVolumeControl();
+  const { volume, setVolume } = usePlayerStore();
+
+  const handleVolumeChange = (value: number) => {
+    setVolume(value / 100);
+  };
 
   return (
-    <div className="volume-control" ref={controlRef}>
-      <div className="desktop-volume">
-        <Volume2 className="controls-icon" />
-        <Slider
-          value={volume * 100}
-          onChange={handleVolumeChange}
-          max={100}
-          step={1}
-          orientation="horizontal"
-          aria-label="Volume"
-        />
-      </div>
-
-      <div className="mobile-volume">
-        <button
-          className="volume-button"
-          onClick={() => setIsOpen(!isOpen)}
-          aria-label="Volume control"
-        >
-          <Volume2 className="controls-icon" />
-        </button>
-
-        <div className={`volume-slider-popup ${isOpen ? "show" : ""}`}>
-          <div className="volume-slider-wrapper">
-            <Slider
-              value={volume * 100}
-              onChange={handleVolumeChange}
-              max={100}
-              step={1}
-              orientation="vertical"
-              aria-label="Volume"
-            />
-          </div>
-        </div>
-      </div>
+    <div className="volume-control">
+      <Volume2 className="controls-icon" />
+      <Slider
+        value={volume * 100}
+        onChange={handleVolumeChange}
+        max={100}
+        step={1}
+        orientation="horizontal"
+        className="volume-slider"
+        aria-label="Volume"
+      />
     </div>
   );
 };
